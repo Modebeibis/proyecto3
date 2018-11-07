1cc5276b5bc3836360efe20ed87d089ecc251753
ramseslopez <ramseslopez99@ciencias.unam.mx>
2018-11-07 03:38:38 -0600

    Mejorando algunas cosas

--- RedInvestigadores/blog/views.py
+++ RedInvestigadores/blog/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render
-from django.utils import timezone
-from .models import Post
 
-def log_in(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/log_in.html')
 
 def home(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/home.html')
+
+def log_in(request):
+    return render(request, 'blog/log_in.html')
