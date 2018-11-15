6c4aa970eb04e3d0a8a3cb6d040f5a254e3f0c1d
Japodrilo <japo@ciencias.unam.mx>
2018-11-14 19:26:24 -0600

    core/templates/blog/ renamed to core/templates/core/. Search html added.

--- RedInvestigadores/core/views.py
+++ RedInvestigadores/core/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'blog/home.html')
+    return render(request, 'core/home.html')
 
 def log_in(request):
-    return render(request, 'blog/log_in.html')
+    return render(request, 'core/log_in.html')
 
 def research(request):
-    return render(request, 'blog/researcher.html')
+    return render(request, 'core/researcher.html')
 
 def about_of(request):
-    return render(request, 'blog/about_of.html')
+    return render(request, 'core/about_of.html')
+
+def search_view(request):
+    return render(request, 'core/search_view.html')
