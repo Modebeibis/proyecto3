836dd3af0f5d906b169aaaca0bba48c3f7f8a0a4
GrayCentipede <maucarrui@gmail.com>
2018-12-13 00:36:06 -0600

    Fixed uniqueness on journal seeder

--- RedInvestigadores/core/seeds/JournalSeeder.py
+++ RedInvestigadores/core/seeds/JournalSeeder.py
@@ -3,12 +3,24 @@ from core.models import Journal
 from random import randint
 
 class JournalSeeder(object):
-    def seed(self):
-        faker = Faker()
-
-        for i in range(50):
+    def get_unique_name(self):
+        faker   = Faker()
+        while True:
             name = faker.company()
+            if not CustomUser.objects.filter(name = name).exists():
+                return name
+
+    def get_unique_issn(self):
+        faker   = Faker()
+        while True:
             issn = faker.isbn10()
+            if not Person.objects.filter(issn = issn).exists():
+                return issn
+
+    def seed(self):
+        for i in range(50):
+            name = self.get_unique_name()
+            issn = self.get_unique_issn()
 
             Journal.objects.get_or_create(name = name,
                                           issn = issn)
